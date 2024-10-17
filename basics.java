a677ca1c20f500edc8074dc94e9763a683d96dbe
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-10-17 21:51:05 +0530

    Add files via upload

--- basics.java
+++ basics.java
@@ -1,30 +1,30 @@
-package oops.inheritence;
+package oops.interfaces;
 
-// Superclass: Animal
-class Animal {
-    // Method in the Animal class
-    public void makeSound() {
-        System.out.println("The animal makes a sound.");
-    }
+interface bicycle {
+    void applyBrake(int decrement);
+
+    void speedup(int increment);
+
 }
 
-// Subclass: Dog inherits from Animal
-class Dog extends Animal {
-    // Method specific to the Dog class
-    public void bark() {
-        System.out.println("The dog barks.");
+class Avon implements bicycle {
+    int speed = 10;
+
+    public void applyBrake(int decrement) {
+        speed = speed - decrement;
+    }
+
+    public void speedup(int increment) {
+        speed = speed + increment;
     }
 }
 
 public class basics {
     public static void main(String[] args) {
-        // Create an instance of Dog
-        Dog dog = new Dog();
+        Avon myBike = new Avon();
+        myBike.speedup(5);
+        myBike.applyBrake(2);
+        System.out.println("Current speed: " + myBike.speed);
 
-        // Call the inherited method from the Animal class
-        dog.makeSound();
-
-        // Call the method from the Dog class
-        dog.bark();
     }
 }
