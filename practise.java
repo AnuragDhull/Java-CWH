355ba7d281ca8c51bf99e3fa71b263a48e9336ad
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-10-20 12:11:37 +0530

    Add files via upload

--- practise.java
+++ practise.java
@@ -1,40 +1,32 @@
-package oops.constructors;
+abstract class pen {
+    abstract public void write();
 
-class Cylinder {
-    int radius;
-    int height;
+    abstract public void refill();
 
-    public Cylinder(int x, int y) {
-        height = x;
-        radius = y;
+}
+
+class fountain extends pen {
+    public void write() {
+        System.out.println("writinggg !!");
     }
 
-    public void setRadius(int i) {
-        radius = i;
+    public void refill() {
+        System.out.println("fillllinggggg");
     }
 
-    public int getRadius() {
-        return radius;
-    }
-
-    public void setHeight(int j) {
-        radius = j;
-    }
-
-    public int getHeight() {
-        return radius;
+    public void changeRefill() {
+        System.out.println("changinggg ");
     }
 
 }
 
 public class practise {
     public static void main(String[] args) {
-        Cylinder cyl = new Cylinder(10, 10);
+        fountain f1 = new fountain();
 
-        // cyl.setRadius(10);
-        // cyl.setHeight(10);
+        f1.changeRefill();
+        f1.write();
+        f1.refill();
 
-        System.out.println(cyl.getRadius());
-        System.out.println(cyl.getHeight());
     }
 }
